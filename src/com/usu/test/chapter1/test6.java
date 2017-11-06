8a7dafe75c36a89cc62b0b9b7cd462706eb36f30
minhld <ledinhminh3883@gmail.com>
2017-11-05 23:44:49 -0700

    added test 6 & 7 (on-going)

---
+++ src/com/usu/test/chapter1/test6.java
@@ -0,0 +1,11 @@
+package com.usu.test.chapter1;
+
+public class test6 extends Thread {
+	public void run() {
+		
+	}
+	
+	public static void main(String args[]) {
+		new test6().start();
+	}
+}
---
+++ src/com/usu/test/chapter1/test7.java
@@ -0,0 +1,11 @@
+package com.usu.test.chapter1;
+
+public class test7 extends Thread {
+	public void run() {
+		
+	}
+	
+	public static void main(String args[]) {
+		new test7().start();
+	}
+}
