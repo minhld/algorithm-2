75ce2c62164664dc2d40e29025202824005d5969
Le Dinh Minh <ledinhminh3883@gmail.com>
2017-11-15 18:24:32 -0700

    adding heap (on-going)

--- src/com/usu/structs/test/testHeap.java
+++ src/com/usu/structs/test/testHeap.java
@@ -2,7 +2,11 @@ package com.usu.structs.test;
 
 public class testHeap extends Thread {
 	public void run() {
-		
+		testExistingHeap();
+	}
+	
+	private void testExistingHeap() {
+		Heap
 	}
 	
 	public static void main(String args[]) {
