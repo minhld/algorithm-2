60e5b7e78cd76af6ab37e1ef1fc41191ffb7b8fc
Le Dinh Minh <ledinhminh3883@gmail.com>
2017-11-18 18:18:30 -0700

    studying dynamic programming (on-going)

---
+++ src/com/usu/test/dp/test1_string.java
@@ -0,0 +1,15 @@
+package com.usu.test.dp;
+
+public class test1_string extends Thread {
+	public void run() {
+		
+	}
+	
+	public void findLongestCommonString(String s1, String s2) {
+		
+	}
+	
+	public static void main(String args[]) {
+		new test1_string().start();
+	}
+}
