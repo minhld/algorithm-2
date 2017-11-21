4738d04f5bb7adb3fb42bfb9e9eb741469142cc6
Le Dinh Minh <ledinhminh3883@gmail.com>
2017-11-20 18:24:43 -0700

    dp - max length of common string

--- src/com/usu/test/dp/test1_string.java
+++ src/com/usu/test/dp/test1_string.java
@@ -8,11 +8,32 @@ package com.usu.test.dp;
  */
 public class test1_string extends Thread {
 	public void run() {
-		
+		String s1 = "quetzalcoatl";
+		String s2 = "tezcatlipoca";
+		int maxCommon = findLongestCommonString(s1, s2);
+		System.out.println("max length: " + maxCommon);
 	}
 	
-	public void findLongestCommonString(String s1, String s2) {
+	public int findLongestCommonString(String s1, String s2) {
+		int[][] maxs = new int[s1.length()][s2.length()];
 		
+		char c1, c2;
+		int max = 0;
+		for (int i = 0; i < s1.length(); i++) {
+			c1 = s1.charAt(i);
+			for (int j = 0; j < s2.length(); j++) {
+				c2 = s2.charAt(j);
+				if (i == 0 || j == 0) {
+					maxs[i][j] = 0;
+				} else if (c1 == c2) {
+					maxs[i][j] = maxs[i - 1][j - 1] + 1;
+					max = Math.max(max, maxs[i][j]);
+				}
+				System.out.print(maxs[i][j] + " ");
+			}
+			System.out.println();
+		}
+		return max;
 	}
 	
 	public static void main(String args[]) {
